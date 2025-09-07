07e14f1b46843a04cb0a2333a325e0939d29c6b7
JahleelT <jlt9287@nyu.edu>
2025-09-07 03:58:07 -0400

    completed firestore methods

--- frontend/services/firestore.ts
+++ frontend/services/firestore.ts
@@ -1,6 +1,34 @@
 import { db } from './firebase';
-import { doc, getDoc, setDoc, updateDoc, increment, onSnapshot } from 'firebase/firestore';
+import { doc, getDoc, setDoc, increment, onSnapshot } from 'firebase/firestore';
 
-export async function incrementUserClicks(uid:string) 
-export async function getUserClicks(uid:string): Promise<void> 
-export function subscribeToUserClicks(uid:string, callback: (count:number) => void) 
+export async function incrementUserClicks(uid:string) {
+  const userDoc = doc(db, 'clicks', uid);
+  await setDoc(userDoc, {count: increment(1)}, {merge:true});
+};
+
+export async function getUserClicks(uid:string): Promise<number> {
+  const userDoc = doc(db, 'clicks', uid);
+  const snapshot = await getDoc(userDoc);
+
+  if (snapshot.exists()) {
+    return snapshot.data().count || 0;
+  } else {
+    return 0;
+  }
+
+};
+
+export function subscribeToUserClicks(uid:string, callback: (count:number) => void) {
+  const userDoc = doc(db, 'clicks', uid);
+  
+  const unsub = onSnapshot(userDoc, (snapshot) => {
+    if (snapshot.exists()) {
+      callback(snapshot.data().count || 0);
+    } else {
+      callback(0);
+    }
+
+  })
+
+  return unsub;
+}
