2eb1faef6b31e0e29f7c57b2be4aa2c62a11ff7c
JahleelT <jlt9287@nyu.edu>
2025-09-07 03:59:40 -0400

    finished conditional rendering in AuthUI

--- frontend/components/AuthUI.tsx
+++ frontend/components/AuthUI.tsx
@@ -1,4 +1,5 @@
 import { useAuth } from '../src/contexts/AuthContext';
+import { ClickCounter } from './ClickCounter';
 import React, { useState }  from 'react';
 
 export const AuthUI: React.FC = () => {
@@ -14,14 +15,32 @@ export const AuthUI: React.FC = () => {
   return (
     <div>
       { (user) ?  
+      
       /* Logged In UI */
       <div>
-
+        <p>Welcome, {user.email}</p>
+        <ClickCounter/>
+        <button onClick={signOut}>Sign Out</button>
       </div>
       : 
+
       /* Logged Out UI */
       <div>
-
+        <p>Welcome! Please Sign In or Up to Get Started!</p>
+        <input 
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='Email'
+        />
+        <input 
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} 
+          placeholder='Password'
+        />
+        <button onClick={() => signIn(email, password)}>Sign In</button>
+        <button onClick={() => signUp(email, password)}>Sign Up</button>
       </div>
       }
     </div>
