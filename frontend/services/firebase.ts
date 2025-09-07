6235f2e2f854789f62292689ec40c1718f6fdf7e
JahleelT <jlt9287@nyu.edu>
2025-09-07 03:58:27 -0400

    updated imports

--- frontend/services/firebase.ts
+++ frontend/services/firebase.ts
@@ -2,7 +2,7 @@ import { getAuth } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
