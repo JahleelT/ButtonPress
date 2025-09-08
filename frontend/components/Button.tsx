8e4165a99d91a7438ba0ad92d53a2add542a5b0a
JahleelT <jlt9287@nyu.edu>
2025-09-08 01:20:01 -0400

    finalization of Button component

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -6,17 +6,21 @@ interface ButtonProps {
   children: React.ReactNode;
   color?: string;
   textColor?: string;
-  onClick?: () => void;
+  onClick?: (() => void);
 }
 
 
 export const Button: React.FC<ButtonProps> = ({ children, color = "bg-blue-500", textColor = "text-white", onClick }) => {
 
   return (
+
     <button
       onClick={onClick}
       className={`px-4 py-2 ${color} ${textColor} font-semibold rounded-lg shadow-md hover:brightness-110 active:scale-95 transition`}>
         {children}
     </button>
+
   );
 }
+
+export default Button;
