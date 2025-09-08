c4f330a8d4f649f3819a2895fce21f299be14b9d
JahleelT <jlt9287@nyu.edu>
2025-09-08 00:58:09 -0400

    created Button component for modularity

---
+++ frontend/components/Button.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import 'tailwindcss';
+
+
+interface ButtonProps {
+  children: React.ReactNode;
+  color?: string;
+  textColor?: string;
+  onClick?: () => void;
+}
+
+
+export const Button: React.FC<ButtonProps> = ({ children, color = "bg-blue-500", textColor = "text-white", onClick }) => {
+
+  return (
+    <button
+      onClick={onClick}
+      className={`px-4 py-2 ${color} ${textColor} font-semibold rounded-lg shadow-md hover:brightness-110 active:scale-95 transition`}>
+        {children}
+    </button>
+  );
+}
